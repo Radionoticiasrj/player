cd96a35904a9dd520a1442fa983a92275b64d503
Radionoticiasrj <81948110+Radionoticiasrj@users.noreply.github.com>
2021-10-15 17:49:44 -0300

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ var settings = {
     'radio_name': 'Brasil Hits',
 
     // URL of streaming without / on the final, eg: http://streaming.com:8080
-    'url_streaming': 'https://streaming.brasilhits.com/acc',
+    'url_streaming': 'https://listen.openstream.co/5613/audio',
 
     // When choosing icecast, make sure the file 'player.log' in the document root is writeable.
     // Choose between 'shoutcast' or 'icecast'
@@ -20,7 +20,7 @@ var settings = {
 
     // Visit https://api.vagalume.com.br/docs/ to get your API key
     'api_key': '18fe07917957c289983464588aabddfb',
-
+    
     // Set to true to get the last songs played
     'historic': true,
 
